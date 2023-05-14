5980abf19c0457affe119bafe2b483a1f54379a6
84h3p <sincereness@vk.com>
2023-05-14 22:48:33 +0600

    script fix

--- stop.sh
+++ stop.sh
@@ -2,5 +2,5 @@
 
 dir_path="$(dirname $(realpath $0))"
 
-cd "$dir_path"/traefik && docker-compose down
+cd "$dir_path"/traefik && docker compose down
 cd "$dir_path"/zabbix && docker compose -f docker-compose_v3_alpine_pgsql_latest.yaml down
