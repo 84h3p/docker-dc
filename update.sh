4fb99f3a73f93377a598684d9c5ede2f59fd6b1d
84h3p <sincereness@vk.com>
2023-05-14 22:41:57 +0600

    script fix

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 dir_path="$(dirname $(realpath $0))"
 
 cd "$dir_path"/traefik && docker compose pull
-cd "$dir_path"/zabbix && docker compose pull
+cd "$dir_path"/zabbix && docker compose -f docker-compose_v3_alpine_pgsql_latest.yaml pull
