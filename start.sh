04d65288fa4bb990dd8171356988996ac647be37
84h3p <sincereness@vk.com>
2023-05-17 16:13:04 +0600

    script fix

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 dir_path="$(dirname $(realpath $0))"
 
 cd "$dir_path"/traefik && docker compose up -d
-cd "$dir_path"/zabbix && docker compose -f docker-compose_v3_alpine_pgsql_latest.yaml up -d 
-
+cd "$dir_path"/zabbix && docker compose up -d
+cd "$dir_path"/netbox && docker compose up -d
--- stop.sh
+++ stop.sh
@@ -3,4 +3,5 @@
 dir_path="$(dirname $(realpath $0))"
 
 cd "$dir_path"/traefik && docker compose down
-cd "$dir_path"/zabbix && docker compose -f docker-compose_v3_alpine_pgsql_latest.yaml down
+cd "$dir_path"/zabbix && docker compose down
+cd "$dir_path"/netbox && docker compose down
