326ecc8939d513db13c02934921c9f0a8bcc2e99
84h3p <43922329+84h3p@users.noreply.github.com>
2023-05-19 14:11:34 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ dir_path="$(dirname $(realpath $0))"
 cd "$dir_path"/traefik && docker compose pull
 cd "$dir_path"/zabbix && docker compose pull
 cd "$dir_path"/netbox && docker compose pull
+cd "$dir_path"/nextcloud && docker compose pull
