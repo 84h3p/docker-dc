0939312f2e1c14e62526696593de39d0e9c77cd0
84h3p <43922329+84h3p@users.noreply.github.com>
2023-05-19 14:11:15 +0000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -5,3 +5,5 @@ dir_path="$(dirname $(realpath $0))"
 cd "$dir_path"/traefik && docker compose down
 cd "$dir_path"/zabbix && docker compose down
 cd "$dir_path"/netbox && docker compose down
+cd "$dir_path"/nextcloud && docker compose down
+
